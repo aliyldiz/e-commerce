ab5445f2814cb324863995712cc208eb393e2146
aliyildiz <aliyyldiz@gmail.com>
2025-01-18 22:14:25 +0300

    admin and ui infrastructure

---
+++ ECommerceClient/src/app/admin/layout/components/components.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import {ProductsModule} from '../../components/products/products.module';
+import {OrdersModule} from '../../components/orders/orders.module';
+import {CustomersModule} from '../../components/customers/customers.module';
+import {DashboardComponent} from '../../components/dashboard/dashboard.component';
+import {DashboardModule} from '../../components/dashboard/dashboard.module';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductsModule,
+    OrdersModule,
+    CustomersModule,
+    DashboardModule
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ]
+})
+export class ComponentsModule { }
