c3b1b3ba149332e77b908faf6f94440327c177f8
aliyildiz <aliyyldiz@gmail.com>
2025-01-19 01:22:40 +0300

    ui bootstrap jquery added

--- ECommerceClient/src/app/app.component.ts
+++ ECommerceClient/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ECommerceClient';
+
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      $(document).ready(() => {
+        alert("Hello World");
+      });
+    }
+  }
 }
