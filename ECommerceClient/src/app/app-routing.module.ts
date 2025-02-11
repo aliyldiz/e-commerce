83c8821229f5577c25e7200ec952986db015f8ca
aliyildiz <aliyyldiz@gmail.com>
2025-02-11 19:06:18 +0300

    creating role based authorization page

--- ECommerceClient/src/app/app-routing.module.ts
+++ ECommerceClient/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
       { path: "customers", loadChildren: () => import('./admin/components/customers/customers.module').then(m => m.CustomersModule), canActivate: [AuthGuard] },
       { path: "products", loadChildren: () => import('./admin/components/products/products.module').then(m => m.ProductsModule), canActivate: [AuthGuard] },
       { path: "orders", loadChildren: () => import('./admin/components/orders/orders.module').then(m => m.OrdersModule), canActivate: [AuthGuard] },
+      { path: "authorize-menu", loadChildren: () => import('./admin/components/authorize-menu/authorize-menu.module').then(m => m.AuthorizeMenuModule), canActivate: [AuthGuard] },
     ], canActivate: [AuthGuard]
   },
   { path: "", component: HomeComponent },
